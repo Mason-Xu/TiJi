654222da932f24c6748844b231614e673b4223fc
Mason-Xu <xulihao1997@gmail.com>
2018-05-23 20:20:01 +0800

    Init a new App : a tools for weight

---
+++ app/src/main/java/xyz/masonxu/tiji/MainActivity.java
@@ -0,0 +1,126 @@
+package xyz.masonxu.tiji;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+
+    private MyFragmentPagerAdapter myFragmentPagerAdapter;
+
+    private TabLayout.Tab one;
+    private TabLayout.Tab two;
+    private TabLayout.Tab three;
+    private TabLayout.Tab four;
+    private TabLayout.Tab five;
+
+
+    private Record_Info[] records = {new Record_Info(65,"2018")};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //初始化视图
+        initViews();
+    }
+
+    private void initViews() {
+
+        //使用适配器将ViewPager与Fragment绑定在一起
+        mViewPager= (ViewPager) findViewById(R.id.viewPager);
+        myFragmentPagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(myFragmentPagerAdapter);
+
+        //将TabLayout与ViewPager绑定在一起
+        mTabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        mTabLayout.setupWithViewPager(mViewPager);
+
+        //指定Tab的位置
+        one = mTabLayout.getTabAt(0);
+        two = mTabLayout.getTabAt(1);
+        three = mTabLayout.getTabAt(2);
+        four = mTabLayout.getTabAt(3);
+        five = mTabLayout.getTabAt(4);
+
+
+        //设置Tab的图标
+        one.setIcon(R.drawable.ic_bmi2);
+        two.setIcon(R.drawable.ic_whtr);
+        three.setIcon(R.drawable.ic_whr);
+        four.setIcon(R.drawable.ic_tizhi2);
+        five.setIcon(R.drawable.ic_shengcai);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.setting_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.setting:
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, setting.class);
+                startActivityForResult(intent, 0);
+                break;
+            case R.id.help:
+                Intent intent2 = new Intent();
+                intent2.setClass(MainActivity.this, help.class);
+                startActivityForResult(intent2, 0);
+                break;
+            case R.id.about:
+                // 使用LayoutInflater来加载dialog_setname.xml布局
+                LayoutInflater layoutInflater = LayoutInflater.from(this);
+                View nameView = layoutInflater.inflate(R.layout.dialog_about, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        this);
+
+                // 使用setView()方法将布局显示到dialog
+                alertDialogBuilder.setView(nameView);
+
+                // 设置Dialog按钮
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("确定",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int id) {
+
+                                    }
+                                });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+                break;
+
+            default:
+        }
+        return true;
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/xyz/masonxu/tiji/MyFragmentPagerAdapter.java
@@ -0,0 +1,43 @@
+package xyz.masonxu.tiji;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Carson_Ho on 16/7/22.
+ */
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    private String[] mTitles = new String[]{"BMI", "腰围身高比", "腰臀比","体脂率","最佳身材"};
+
+    public MyFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 1) {
+            return new Fragment2();
+        } else if (position == 2) {
+            return new Fragment3();
+        }else if (position == 3) {
+            return new Fragment4();
+        }else if (position == 4) {
+            return new Fragment5();
+        }
+
+        return new Fragment1();
+    }
+
+    @Override
+    public int getCount() {
+        return mTitles.length;
+    }
+
+    //ViewPager与TabLayout绑定后，这里获取到PageTitle就是Tab的Text
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mTitles[position];
+    }
+}
